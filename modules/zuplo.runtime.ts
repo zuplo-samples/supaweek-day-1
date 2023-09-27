549947ed086781710901a63f8d191164ea5be3d8
aabedraba <aabedraba@gmail.com>
2023-09-27 23:10:45 +0200

    add exhaustive check for env variables

---
+++ modules/zuplo.runtime.ts
@@ -0,0 +1,57 @@
+// This file is used to check that you have setup your project's
+// environment variables correctly. It is not necessary in most
+// projects but demonstrates the power of Zuplo's extensibility!
+
+// See the docs for this file at
+// https://zuplo.com/docs/articles/runtime-extensions
+
+import { RuntimeExtensions, environment } from "@zuplo/runtime";
+
+let verified = false;
+
+const requiredEnvVars = [
+  "OPENAI_API_KEY",
+  "SUPABASE_URL",
+  "SUPABASE_SERVICE_ROLE_KEY",
+];
+
+function verify(): string | undefined {
+  if (verified) {
+    return;
+  }
+
+  const missingKeys: string[] = [];
+
+  requiredEnvVars.forEach((key) => {
+    if (!environment[key]) {
+      missingKeys.push(key);
+    }
+  });
+
+  if (missingKeys.length > 0) {
+    return `Your project is missing some required Environment Variables: '${missingKeys.join(
+      ", "
+    )}'`;
+  }
+
+  verified = true;
+}
+
+export function runtimeInit(runtime: RuntimeExtensions) {
+  runtime.addRequestHook(async (request, context) => {
+    const url = new URL(request.url);
+
+    // don't intercept CORS, that makes debugging hard
+    // in the browser. Also, don't mess with built-in routes
+    if (request.method === "OPTIONS") {
+      return request;
+    }
+
+    const result = verify();
+    if (result) {
+      return new Response(result, { status: 500 });
+    }
+
+    return request;
+  });
+}
